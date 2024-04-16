785cafb1d2957898093c22f8f3f40ab520a3c6f8
Tolentino-JP <johnpaultolentino5@gmail.com>
2024-04-16 14:12:22 +0800

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,50 @@
+// Press Login
+
+let loginContent = document.querySelector(".login-content");
+let openLogin = document.querySelector(".navigation");
+let closeModal = document.querySelector(".close-modal");
+let blurBg = document.querySelector(".blur-bg");
+
+openLogin.addEventListener("click", function(){
+    loginContent.classList.remove("hide-login");
+    blurBg.classList.remove("hide-blur");
+});
+
+closeModal.addEventListener( "click" ,function() {
+    loginContent.classList.add("hide-login");
+    blurBg.classList.add("hide-blur");
+});
+
+
+
+
+
+
+
+
+
+// enter username and  password
+
+document.getElementById('submit').addEventListener('click', function(event){
+
+        event.preventDefault(); // Prevent the form from submitting
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+
+        login(username, password);
+
+    }
+
+);
+
+function login(username, password){
+
+    if(username == "John" && password == "Tolentino1552"){
+        console.log("Correct Username!");
+        console.log("Correct Password!");   
+    }else{
+        document.getElementById("error").style.display="inline";
+    }
+    
+
+}
